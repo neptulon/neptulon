f7df7c343f72fe79ce9966badef7a78f83c25721
Teoman Soygul <tsoygul@gmail.com>
2015-06-12 13:32:25 +0200

    add jsonrpc client template

---
+++ jsonrpc/client.go
@@ -0,0 +1,6 @@
+package jsonrpc
+
+// Client is a client implementation for JSON-RPC 2.0 protocol for Neptulon framework.
+// Client implementations in other programming languages might be provided in separate repositories so check the documentation.
+type Client struct {
+}
