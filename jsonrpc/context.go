67140009e137bf228aa7b007e4194d68e1b51d86
Teoman Soygul <tsoygul@gmail.com>
2015-06-17 11:53:24 +0200

    switch router to use context

--- jsonrpc/context.go
+++ jsonrpc/context.go
@@ -4,32 +4,32 @@ import "github.com/nbusy/neptulon"
 
 // ReqContext encapsulates connection, request, and reponse objects for a JSON-RPC request.
 type ReqContext struct {
-	conn    *neptulon.Conn
-	req     *Request
-	res     interface{}
-	resErr  *ResError
+	Conn    *neptulon.Conn
+	Req     *Request
+	Res     interface{}
+	ResErr  *ResError
 	handled bool
 }
 
-// Res returns the response object if it was set.
-func (r *ReqContext) Res() interface{} {
-	return nil
-}
-
-// SetRes sets the response object and marks the request handled.
-func (r *ReqContext) SetRes(res interface{}) {
-	r.handled = true
-}
-
-// Handled returns true if a response was set or if the request was explicitly marked handled.
-func (r *ReqContext) Handled() bool {
-	return r.handled
-}
-
-// SetHandled marks the request as handled. This is automatically done when SetRes is used.
-func (r *ReqContext) SetHandled() bool {
-	return r.handled
-}
+// // Res returns the response object if it was set.
+// func (r *ReqContext) Res() interface{} {
+// 	return nil
+// }
+//
+// // SetRes sets the response object and marks the request handled.
+// func (r *ReqContext) SetRes(res interface{}) {
+// 	r.handled = true
+// }
+//
+// // Handled returns true if a response was set or if the request was explicitly marked handled.
+// func (r *ReqContext) Handled() bool {
+// 	return r.handled
+// }
+//
+// // SetHandled marks the request as handled. This is automatically done when SetRes is used.
+// func (r *ReqContext) SetHandled() bool {
+// 	return r.handled
+// }
 
 // NotContext encapsulates connection and notification objects for a JSON-RPC notification.
 type NotContext struct {
--- jsonrpc/router.go
+++ jsonrpc/router.go
@@ -4,15 +4,15 @@ import "github.com/nbusy/neptulon"
 
 // Router is a JSON-RPC request routing middleware.
 type Router struct {
-	requestRoutes      map[string]func(conn *neptulon.Conn, req *Request) (result interface{}, err *ResError)
-	notificationRoutes map[string]func(conn *neptulon.Conn, not *Notification)
+	requestRoutes      map[string]func(ctx *ReqContext)
+	notificationRoutes map[string]func(ctx *NotContext)
 }
 
 // NewRouter creates a JSON-RPC router instance and registers it with the Neptulon JSON-RPC app.
 func NewRouter(app *App) (*Router, error) {
 	r := Router{
-		requestRoutes:      make(map[string]func(conn *neptulon.Conn, req *Request) (result interface{}, err *ResError)),
-		notificationRoutes: make(map[string]func(conn *neptulon.Conn, not *Notification)),
+		requestRoutes:      make(map[string]func(ctx *ReqContext)),
+		notificationRoutes: make(map[string]func(ctx *NotContext)),
 	}
 
 	app.Middleware(r.middleware)
@@ -20,12 +20,12 @@ func NewRouter(app *App) (*Router, error) {
 }
 
 // Request adds a new request route registry.
-func (r *Router) Request(route string, handler func(conn *neptulon.Conn, req *Request) (result interface{}, err *ResError)) {
+func (r *Router) Request(route string, handler func(ctx *ReqContext)) {
 	r.requestRoutes[route] = handler
 }
 
 // Notification adds a new notification route registry.
-func (r *Router) Notification(route string, handler func(conn *neptulon.Conn, not *Notification)) {
+func (r *Router) Notification(route string, handler func(ctx *NotContext)) {
 	r.notificationRoutes[route] = handler
 }
 
@@ -38,13 +38,15 @@ func (r *Router) middleware(conn *neptulon.Conn, msg *Message) (result interface
 	// if request
 	if msg.ID != "" {
 		if handler, ok := r.requestRoutes[msg.Method]; ok {
-			if res, resErr := handler(conn, &Request{ID: msg.ID, Method: msg.Method, Params: msg.Params}); res != nil || resErr != nil {
-				return res, resErr
+			ctx := ReqContext{Conn: conn, Req: &Request{ID: msg.ID, Method: msg.Method, Params: msg.Params}}
+			if handler(&ctx); ctx.Res != nil || ctx.ResErr != nil {
+				return ctx.Res, ctx.ResErr
 			}
 		}
 	} else { // if notification
 		if handler, ok := r.notificationRoutes[msg.Method]; ok {
-			handler(conn, &Notification{Method: msg.Method, Params: msg.Params})
+			ctx := NotContext{conn: conn, not: &Notification{Method: msg.Method, Params: msg.Params}}
+			handler(&ctx)
 			// todo: need to return something to prevent deeper handlers to further handle this request (i.e. not found handler logging not found warning)
 		}
 	}
