2f14f2baedec065be05b003645881027cbf1dd18
Teoman Soygul <tsoygul@gmail.com>
2015-06-17 20:35:36 +0200

    add certauth type

--- jsonrpc/auth.go
+++ jsonrpc/auth.go
@@ -4,26 +4,40 @@ import (
 	"fmt"
 	"log"
 	"strconv"
-
-	"github.com/nbusy/neptulon"
 )
 
-func authMiddleware(conn *neptulon.Conn, msg *Message) {
-	if conn.Session.Get("userid") != 0 {
+// CertAuth is a TLS certificate authentication middleware for Neptulon JSON-RPC app.
+type CertAuth struct {
+}
+
+// NewCertAuth creates and registers a new certificate authentication middleware instance with a Neptulon JSON-RPC app.
+func NewCertAuth(app *App) (*CertAuth, error) {
+	a := CertAuth{}
+	app.Middleware(a.middleware)
+	return &a, nil
+}
+
+func (a *CertAuth) middleware(ctx *Context) {
+	if ctx.Conn.Session.Get("userid") != 0 {
 		return
 	}
 
+	// possible responses:
+	// cert revoked
+	// cert expired
+	// cert invalid
+
 	// client certificate authorization: certificate is verified by the TLS listener instance so we trust it
-	peerCerts := conn.ConnectionState().PeerCertificates
+	peerCerts := ctx.Conn.ConnectionState().PeerCertificates
 	if len(peerCerts) > 0 {
 		idstr := peerCerts[0].Subject.CommonName
 		uid64, err := strconv.ParseUint(idstr, 10, 32)
 		if err != nil {
-			conn.Session.Set("error", fmt.Errorf("Cannot parse client message or method mismatched: %v", err))
+			ctx.Conn.Session.Set("error", fmt.Errorf("Cannot parse client message or method mismatched: %v", err))
 			return
 		}
 		userID := uint32(uid64)
 		log.Printf("Client connected with client certificate subject: %+v", peerCerts[0].Subject)
-		conn.Session.Set("userid", userID)
+		ctx.Conn.Session.Set("userid", userID)
 	}
 }
