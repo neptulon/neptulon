c8c66031948f770024a4c165f51e7adb906dfbc5
Teoman Soygul <tsoygul@gmail.com>
2015-06-21 12:32:28 +0200

    remove user ID parsing during auth

--- jsonrpc/auth.go
+++ jsonrpc/auth.go
@@ -1,9 +1,6 @@
 package jsonrpc
 
-import (
-	"log"
-	"strconv"
-)
+import "log"
 
 // CertAuth is a TLS certificate authentication middleware for Neptulon JSON-RPC app.
 type CertAuth struct {
@@ -24,22 +21,14 @@ func (a *CertAuth) middleware(ctx *Context) {
 	// if provided, client certificate is verified by the TLS listener so the peerCerts list in the connection is trusted
 	certs := ctx.Conn.ConnectionState().PeerCertificates
 	if len(certs) == 0 {
+		// todo: better use sender and send back a notification and close conn immediately as we don't know the type of incoming message at this point
+		// other approach would be to generate a response based on incoming message type
 		ctx.OutMsg = &Message{Error: &ResError{Code: 666, Message: "Invalid client certificate.", Data: certs}}
-		// todo: ctx.Send/Fin/Write/End()
 		ctx.Conn.Close()
 		return
 	}
 
-	idstr := certs[0].Subject.CommonName
-	uid64, err := strconv.ParseUint(idstr, 10, 32)
-	if err != nil {
-		ctx.OutMsg = &Message{Error: &ResError{Code: 666, Message: "Invalid client certificate.", Data: certs}}
-		// todo: ctx.Send/Fin/Write/End()
-		ctx.Conn.Close()
-		return
-	}
-
-	userID := uint32(uid64)
+	userID := certs[0].Subject.CommonName
 	ctx.Conn.Session.Set("userid", userID)
 	log.Println("Client-certificate authenticated:", ctx.Conn.RemoteAddr(), userID)
 }
