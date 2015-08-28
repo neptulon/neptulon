cbbbdffd269794cbc24476b1dec9076eeff83354
Teoman Soygul <tsoygul@gmail.com>
2015-08-29 01:06:03 +0200

    merge sender into router

--- jsonrpc/router.go
+++ jsonrpc/router.go
@@ -2,19 +2,24 @@ package jsonrpc
 
 // Router is a JSON-RPC request routing middleware.
 type Router struct {
-	reqRoutes map[string]func(ctx *ReqContext)
-	notRoutes map[string]func(ctx *NotContext)
+	jsonrpc        *App
+	reqRoutes      map[string]func(ctx *ReqContext)
+	notRoutes      map[string]func(ctx *NotContext)
+	pendinRequests map[string]chan *Response // requests sent from the router that are pending responses from clients
 }
 
 // NewRouter creates a JSON-RPC router instance and registers it with the Neptulon JSON-RPC app.
 func NewRouter(app *App) (*Router, error) {
 	r := Router{
-		reqRoutes: make(map[string]func(ctx *ReqContext)),
-		notRoutes: make(map[string]func(ctx *NotContext)),
+		jsonrpc:        app,
+		reqRoutes:      make(map[string]func(ctx *ReqContext)),
+		notRoutes:      make(map[string]func(ctx *NotContext)),
+		pendinRequests: make(map[string]chan *Response),
 	}
 
 	app.ReqMiddleware(r.reqMiddleware)
 	app.NotMiddleware(r.notMiddleware)
+	app.ResMiddleware(r.resMiddleware)
 	return &r, nil
 }
 
@@ -29,6 +34,19 @@ func (r *Router) Notification(route string, handler func(ctx *NotContext)) {
 	r.notRoutes[route] = handler
 }
 
+// SendRequest sends a JSON-RPC request throught the connection denoted by the connection ID.
+func (r *Router) SendRequest(connID string, req *Request) chan<- *Response {
+	r.jsonrpc.Send(connID, req)
+	ch := make(chan *Response)
+	r.pendinRequests[req.ID] = ch
+	return ch
+}
+
+// SendNotification sends a JSON-RPC notification through the connection denoted by the connection ID.
+func (r *Router) SendNotification(connID string, not *Notification) {
+	r.jsonrpc.Send(connID, not)
+}
+
 func (r *Router) reqMiddleware(ctx *ReqContext) {
 	if handler, ok := r.reqRoutes[ctx.Req.Method]; ok {
 		handler(ctx)
@@ -38,5 +56,12 @@ func (r *Router) reqMiddleware(ctx *ReqContext) {
 func (r *Router) notMiddleware(ctx *NotContext) {
 	if handler, ok := r.notRoutes[ctx.Not.Method]; ok {
 		handler(ctx)
+	}
+}
+
+func (r *Router) resMiddleware(ctx *ResContext) {
+	if ch, ok := r.pendinRequests[ctx.Res.ID]; ok {
+		ch <- ctx.Res
+		delete(r.pendinRequests, ctx.Res.ID)
 	}
 }
