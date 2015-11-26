aa2068fbe62f95bdd17af5a973187d4e6fcb6b6c
Teoman Soygul <tsoygul@gmail.com>
2015-11-26 17:07:52 +0100

    use Client in Context instead of Conn

--- client_tls.go
+++ client_tls.go
@@ -1,15 +1,37 @@
 package neptulon
 
+import (
+	"fmt"
+	"log"
+)
+
 // TLSClient is a Neptulon connection client using Transport Layer Security.
 type TLSClient struct {
+	Conn Conn
+
 	// middleware for incoming and outgoing messages
 	in  []func(ctx *Ctx)
 	out []func(ctx *Ctx)
 }
 
 // NewTLSClient creates a new client using a given tls.Conn.
-func NewTLSClient() *TLSClient {
-	return &TLSClient{}
+func NewTLSClient(conn Conn) *TLSClient {
+	return &TLSClient{
+		Conn: conn,
+	}
+}
+
+// Send writes the given message to the connection.
+func (c *TLSClient) Send(msg []byte) error {
+	if err := c.Conn.Write(msg); err != nil {
+		e := fmt.Errorf("Errored while writing response to connection: %v", err)
+		log.Fatalln(e)
+		return e
+	}
+
+	return nil
 }
 
 // todo: add variadic functions to insert in/out msg middleware + interface definitions.
+// move listener handleClient functionality here
+// should writing be queue based on a separate thread or configurable?
--- ctx.go
+++ ctx.go
@@ -1,13 +1,8 @@
 package neptulon
 
-import (
-	"fmt"
-	"log"
-)
-
 // Ctx is the incoming message context.
 type Ctx struct {
-	Conn    Conn
+	Client  Client
 	Msg     []byte
 	Session interface{} // Session is a data store for storing arbitrary data within this context to communicate with middleware further down the stack.
 
@@ -22,17 +17,4 @@ func (ctx *Ctx) Next() {
 	if ctx.mi <= len(ctx.m) {
 		ctx.m[ctx.mi-1](ctx)
 	}
-}
-
-// Send writes the given message to the connection.
-func (ctx *Ctx) Send(msg []byte) error {
-	// todo: neptulon.Send vs Ctx.Send
-
-	if err := ctx.Conn.Write(msg); err != nil {
-		e := fmt.Errorf("Errored while writing response to connection: %v", err)
-		log.Fatalln(e)
-		return e
-	}
-
-	return nil
 }
