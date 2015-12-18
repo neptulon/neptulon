83bd350abe5a1927b5123f8add221006ee717f01
Teoman Soygul <tsoygul@gmail.com>
2015-12-18 01:17:21 +0100

    add test todo

--- test/connect_test.go
+++ test/connect_test.go
@@ -12,18 +12,24 @@ import "testing"
 func TestConnectTLS(t *testing.T) {
 	sh := NewTLSServerHelper(t)
 	defer sh.Close()
-	// sh.GetTLSClient(true)
-	// defer ch.Close()
+	ch := sh.GetTLSClient(true)
+	defer ch.Close()
 
+	// todo: solve multiple close error messages first
+
+	// var wg sync.WaitGroup
 	// msg := []byte("test message")
 	//
 	// sh.Server.MiddlewareIn(middleware.Echo)
 	// ch.Client.MiddlewareIn(func(ctx *client.Ctx) {
+	// 	defer wg.Done()
 	// 	if !reflect.DeepEqual(ctx.Msg, msg) {
 	// 		t.Fatalf("expected: '%s', got: '%s'", msg, ctx.Msg)
 	// 	}
 	// 	ctx.Next()
 	// })
 	//
+	// wg.Add(1)
 	// ch.Client.Send(msg) // todo: use fail-safe ClientHelper.Send instead
+	// wg.Wait()
 }
