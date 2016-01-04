bf07043ea7945a9cae1a59f9fc901f87e97331e2
Teoman Soygul <tsoygul@gmail.com>
2016-01-04 14:52:01 +0100

    add optional tls config

--- server.go
+++ server.go
@@ -2,6 +2,10 @@
 package neptulon
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -16,6 +20,7 @@ type Server struct {
 	addr       string
 	conns      *cmap.CMap // conn ID -> Conn
 	middleware []func(ctx *ReqCtx) error
+	wsConfig   websocket.Config
 }
 
 // NewServer creates a new Neptulon server.
@@ -26,6 +31,36 @@ func NewServer(addr string) *Server {
 	}
 }
 
+// UseTLS enables Transport Layer Security for the connection.
+// cert, key = Server certificate/private key pair.
+// clientCACert = Optional certificate for verifying client certificates.
+// All certificates/private keys are in PEM encoded X.509 format.
+func (s *Server) UseTLS(cert, privKey, clientCACert []byte) error {
+	tlsCert, err := tls.X509KeyPair(cert, privKey)
+	if err != nil {
+		return fmt.Errorf("failed to parse the server certificate or the private key: %v", err)
+	}
+
+	c, _ := pem.Decode(cert)
+	if tlsCert.Leaf, err = x509.ParseCertificate(c.Bytes); err != nil {
+		return fmt.Errorf("failed to parse the server certificate: %v", err)
+	}
+
+	pool := x509.NewCertPool()
+	ok := pool.AppendCertsFromPEM(clientCACert)
+	if !ok {
+		return fmt.Errorf("failed to parse the CA certificate: %v", err)
+	}
+
+	s.wsConfig.TlsConfig = &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		ClientCAs:    pool,
+		ClientAuth:   tls.VerifyClientCertIfGiven,
+	}
+
+	return nil
+}
+
 // Middleware registers middleware to handle incoming request messages.
 func (s *Server) Middleware(middleware ...func(ctx *ReqCtx) error) {
 	s.middleware = append(s.middleware, middleware...)
@@ -34,7 +69,8 @@ func (s *Server) Middleware(middleware ...func(ctx *ReqCtx) error) {
 // Start the Neptulon server. This function blocks until server is closed.
 func (s *Server) Start() error {
 	http.Handle("/", websocket.Server{
-		Handler: s.connHandler,
+		Config:  s.wsConfig,
+		Handler: s.wsHandler,
 		Handshake: func(config *websocket.Config, req *http.Request) error {
 			config.Origin, _ = url.Parse(req.RemoteAddr) // we're interested in remote address and not origin header text
 			return nil
@@ -44,7 +80,7 @@ func (s *Server) Start() error {
 	return http.ListenAndServe(s.addr, nil)
 }
 
-func (s *Server) connHandler(ws *websocket.Conn) {
+func (s *Server) wsHandler(ws *websocket.Conn) {
 	log.Println("Client connected:", ws.RemoteAddr())
 	c, err := NewConn(ws, s.middleware)
 	if err != nil {
@@ -57,3 +93,20 @@ func (s *Server) connHandler(ws *websocket.Conn) {
 	s.conns.Delete(c.ID)
 	log.Println("Connection closed:", ws.RemoteAddr())
 }
+
+// Close closes the network listener and the active connections.
+// func (s *Server) Close() error {
+// 	err := s.listener.Close()
+//
+// 	// close all active connections discarding any read/writes that is going on currently
+// 	// this is not a problem as we always require an ACK but it will also mean that message deliveries will be at-least-once; to-and-from the server
+// 	s.clients.Range(func(c interface{}) {
+// 		c.(*Client).Close()
+// 	})
+//
+// 	if err != nil {
+// 		return fmt.Errorf("And error occured before or while stopping the server: %v", err)
+// 	}
+//
+// 	return nil
+// }
