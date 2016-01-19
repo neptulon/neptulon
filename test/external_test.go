7e5a81508004ef2af5bbea872582e3c2b1ec98ee
Teoman Soygul <tsoygul@gmail.com>
2016-01-19 16:37:59 +0100

    add external test case template

---
+++ test/external_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/neptulon/neptulon"
+	"github.com/neptulon/neptulon/middleware"
+)
+
+var ext = flag.Bool("ext", false, "Run external client test case.")
+
+// Helper method for testing client implementations in other languages.
+// Flow of events for this function is:
+// * Wait to receive any request message.
+// * Echo the message body as a response.
+// * Send an {"method":"echo", "params":"Lorem ip sum..."} request to client.
+// * Wait for response and verify that message body is echoed properly in the response body.
+// * Repeat ad infinitum, until {"method":"close", "params":"..."} is received. Close message body is logged.
+func TestExternalClient(t *testing.T) {
+	if !*ext {
+		t.Skip("Skipping external client integration test since -ext flag is not provided.")
+	}
+
+	sh := NewServerHelper(t).Start()
+	defer sh.Close()
+
+	rout := middleware.NewRouter()
+	sh.Middleware(rout.Middleware)
+	rout.Request("echo", middleware.Echo)
+
+	ch := sh.GetConnHelper().Connect()
+	defer ch.Close()
+
+	ch.SendRequest("echo", echoMsg{Message: "Hello!"}, func(ctx *neptulon.ResCtx) error {
+		var msg echoMsg
+		if err := ctx.Result(&msg); err != nil {
+			t.Fatal(err)
+		}
+		if msg.Message != "Hello!" {
+			t.Fatalf("expected: %v got: %v", "Hello!", msg.Message)
+		}
+		return nil
+	})
+}
