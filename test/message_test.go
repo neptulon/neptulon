d6192e26fbd811d864786a9924d951c24c985474
Teoman Soygul <tsoygul@gmail.com>
2016-01-27 21:26:12 +0100

    reorder test cases

--- test/message_test.go
+++ test/message_test.go
@@ -24,44 +24,11 @@ var (
 	msg5 = randstr.Get(5 * 1000 * 1000) // 5.0 MB
 )
 
-func TestMessages(t *testing.T) {
-	// todo: verify all message echoes from small to big
-}
-
-func TestBidirectional(t *testing.T) {
-	// todo: test simultaneous read/writes
-}
-
-func TestEcho(t *testing.T) {
-	sh := NewServerHelper(t)
-	rout := middleware.NewRouter()
-	sh.Middleware(middleware.Logger)
-	sh.Middleware(rout.Middleware)
-	rout.Request("echo", middleware.Echo)
-	defer sh.Start().CloseWait()
-
-	ch := sh.GetConnHelper()
-	ch.Middleware(middleware.Logger)
-	defer ch.Connect().CloseWait()
-
-	m := "Hello!"
-	ch.SendRequest("echo", echoMsg{Message: m}, func(ctx *neptulon.ResCtx) error {
-		var msg echoMsg
-		if err := ctx.Result(&msg); err != nil {
-			t.Fatal(err)
-		}
-		if msg.Message != m {
-			t.Fatalf("expected: %v got: %v", m, msg.Message)
-		}
-		return nil
-	})
-}
-
 func TestEchoWithoutTestHelpers(t *testing.T) {
 	s := neptulon.NewServer("127.0.0.1:3001")
 	go s.Start()
-	defer s.Close()
 	time.Sleep(time.Millisecond)
+	defer s.Close()
 
 	var wg sync.WaitGroup
 	s.Middleware(func(ctx *neptulon.ReqCtx) error {
@@ -89,6 +56,39 @@ func TestEchoWithoutTestHelpers(t *testing.T) {
 	t.Log("Got response:", res)
 
 	wg.Wait()
+}
+
+func TestEcho(t *testing.T) {
+	sh := NewServerHelper(t)
+	rout := middleware.NewRouter()
+	sh.Middleware(middleware.Logger)
+	sh.Middleware(rout.Middleware)
+	rout.Request("echo", middleware.Echo)
+	defer sh.Start().CloseWait()
+
+	ch := sh.GetConnHelper()
+	ch.Middleware(middleware.Logger)
+	defer ch.Connect().CloseWait()
+
+	m := "Hello!"
+	ch.SendRequest("echo", echoMsg{Message: m}, func(ctx *neptulon.ResCtx) error {
+		var msg echoMsg
+		if err := ctx.Result(&msg); err != nil {
+			t.Fatal(err)
+		}
+		if msg.Message != m {
+			t.Fatalf("expected: %v got: %v", m, msg.Message)
+		}
+		return nil
+	})
+}
+
+func TestMessages(t *testing.T) {
+	// todo: verify all message echoes from small to big
+}
+
+func TestBidirectional(t *testing.T) {
+	// todo: test simultaneous read/writes
 }
 
 func TestTLS(t *testing.T) {
