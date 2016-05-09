77a26b85a1d3599e74d862b9212dc9d1811f1cb7
Teoman Soygul <tsoygul@gmail.com>
2016-05-09 15:42:31 +0200

    add TestMiddlewareErrorReturn impl

--- test/middleware_test.go
+++ test/middleware_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"errors"
 	"log"
 	"testing"
 	"time"
@@ -33,9 +34,34 @@ func TestMiddlewarePanic(t *testing.T) {
 	case <-time.After(time.Millisecond * 25):
 	}
 
+	// todo: rather than waiting 25 millisecs, add a Conn.Disconnected handler and check that it is called
 	// todo: verify that the server is still up and functional
 }
 
 func TestMiddlewareErrorReturn(t *testing.T) {
+	sh := NewServerHelper(t)
+	sh.Server.MiddlewareFunc(middleware.Logger)
+	sh.Server.MiddlewareFunc(func(ctx *neptulon.ReqCtx) error {
+		return errors.New("much error")
+	})
+	sh.Server.MiddlewareFunc(middleware.Echo)
+	defer sh.ListenAndServe().CloseWait()
 
+	ch := sh.GetConnHelper().Connect()
+	defer ch.CloseWait()
+
+	gotRes := make(chan bool)
+	ch.Conn.SendRequest("echo", echoMsg{Message: "just testing"}, func(ctx *neptulon.ResCtx) error {
+		if ctx.ErrorMessage == "" || ctx.ErrorCode == 0 {
+			t.Fatal("expected error response with message, got none")
+		}
+		gotRes <- true
+		return nil
+	})
+
+	select {
+	case <-gotRes:
+	case <-time.After(time.Millisecond * 100):
+		log.Fatal("expected error response, got none")
+	}
 }
